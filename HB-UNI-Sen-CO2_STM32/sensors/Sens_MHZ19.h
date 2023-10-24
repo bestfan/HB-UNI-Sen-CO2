ca7e61782c9ba3e29d175cca0f5db6f85c20b0c2
bestfan <59401147+bestfan@users.noreply.github.com>
2023-10-24 21:08:14 +0200

    Update Sens_MHZ19.h
    Can now be used without changes to the MH-Z19 library.

--- HB-UNI-Sen-CO2_STM32/sensors/Sens_MHZ19.h
+++ HB-UNI-Sen-CO2_STM32/sensors/Sens_MHZ19.h
@@ -1,5 +1,5 @@
 //---------------------------------------------------------
-// 2022-04-27 bestfan (cc)
+// 2023-10-24 bestfan (cc)
 // Sens_MHZ19 based on Sens_SCD30 by.
 // 2019-07-14 Tom Major (Creative Commons)
 // https://creativecommons.org/licenses/by-nc-sa/4.0/
@@ -34,17 +34,21 @@ class Sens_MHZ19 : public Sensor {
     void measureRaw()
     {
         DPRINTLN("");
-        while (!_mhz19.available())
+        _mhz19.verify();
+        while (_mhz19.errorCode != RESULT_OK)
         {
             delay(1000);
             DPRINT(".");
-           DPRINTLN("Error: no Winsen MHZ19 CO2 sensor found");
-           if (_mhz19.available())
-            {
-              break;
-            }
+            DPRINTLN("Error: no Winsen MHZ19 CO2 sensor found");
+//           if (_mhz19.available())
+            _mhz19.verify();
+            if (_mhz19.errorCode == RESULT_OK)
+	    {
+	      break;
+	    }
         }
-        if (_mhz19.available())
+        _mhz19.verify();
+        if (_mhz19.errorCode == RESULT_OK)
         {
               _carbondioxide    = (uint16_t)(_mhz19.getCO2(true, true));
               _temperature      = (int16_t)(_mhz19.getTemperature(true, false)); 
@@ -68,8 +72,11 @@ public:
         //_scd30.sendCommand(COMMAND_SOFT_RESET);
         //delay(200);
         _mhz19.begin(co2Serial);
-        if (_mhz19.available() == false)
+//       if (_mhz19.available() == false)
+        _mhz19.verify();
+        if (_mhz19.errorCode != RESULT_OK) 
         {
+
           DPRINTLN("Error: no Winsen MHZ19 CO2 sensor found");
         }
         else
@@ -83,9 +90,8 @@ public:
 
     bool setForcedRecalibrationFactor(uint16_t concentration)
     {
-        //concentration not used for MH-Z19
-        _mhz19.calibrate();
-        return 1;
+//      return(_mhz19.setForcedRecalibrationFactor(concentration));
+        return 0;
     }
 
 
@@ -127,69 +133,3 @@ public:
 }
 
 #endif
-
-
-/*   Version: 1.5.3  |  License: LGPLv3  |  Author: JDWifWaf@gmail.com   */
-/*   same as verify fuction in MH-Z19 library but with boolean return    */
-/*   by bestfan 2022-04-27                                               */
-
-bool MHZ19::available()
-{
-    unsigned long timeStamp = millis();
-
-    /* construct common command (133) */
-    constructCommand(CO2UNLIM);
-
-    write(this->storage.constructedCommand);
-
-    while (read(this->storage.responses.CO2UNLIM, CO2UNLIM) != RESULT_OK)
-    {
-        if (millis() - timeStamp >= TIMEOUT_PERIOD)
-        {
-           #if defined (ESP32) && (MHZ19_ERRORS)
-            ESP_LOGE(TAG_MHZ19, "Failed to verify connection(1) to sensor.");   
-            #elif MHZ19_ERRORS
-            Serial.println("!ERROR: Failed to verify connection(1) to sensor.");
-            #endif   
-
-            return 0;
-        }
-    }
-
-    /* construct & write last response command (162) */
-    constructCommand(GETLASTRESP);
-    write(this->storage.constructedCommand);
-    
-    /* update timeStamp  for next comms iteration */ 
-    timeStamp = millis();
-
-    while (read(this->storage.responses.STAT, GETLASTRESP) != RESULT_OK)
-    {
-        if (millis() - timeStamp >= TIMEOUT_PERIOD)
-        {
-            #if defined (ESP32) && (MHZ19_ERRORS)
-            ESP_LOGE(TAG_MHZ19, "Failed to verify connection(2) to sensor.");   
-            #elif MHZ19_ERRORS
-            Serial.println("!ERROR: Failed to verify connection(2) to sensor.");
-            #endif
-            
-            return 0;
-        }
-    }      
-
-    /* compare CO2 & temp bytes, command(133), against last response bytes, command (162)*/
-    for (byte i = 2; i < 6; i++)
-    {
-        if (this->storage.responses.CO2UNLIM[i] != this->storage.responses.STAT[i])
-        {
-            #if defined (ESP32) && (MHZ19_ERRORS)
-            ESP_LOGE(TAG_MHZ19, "Last response is not as expected, verification failed.");   
-            #elif MHZ19_ERRORS
-            Serial.println("!ERROR: Last response is not as expected, verification failed.");
-            #endif
-
-            return 0;
-        }
-    }
-    return 1;
-}
